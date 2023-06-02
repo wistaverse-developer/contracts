78419be9c62b362b8e7e8093775b7e26b3c26ee7
Yves-Marie Saout <ymsaout42@gmail.com>
2023-06-02 18:12:35 +0200

    Add notice

--- contracts/Wistake.sol
+++ contracts/Wistake.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-
-contract RewardToken is ERC20, Ownable {
+/// @notice The constructor for the Wistake Token.
+contract Wistake is ERC20, Ownable {
     constructor() ERC20("Wistake", "SWISTA") {
         _mint(msg.sender, 42 * (10**24));
     }
