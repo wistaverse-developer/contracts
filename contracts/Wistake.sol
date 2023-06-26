d66501ec17730dbc1f60ff708409302ba7279e0a
PopMauMau <134159374+PopMauMau@users.noreply.github.com>
2023-06-26 19:11:06 +0200

    Update Wistake.sol

--- contracts/Wistake.sol
+++ contracts/Wistake.sol
@@ -5,7 +5,19 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Wistake is ERC20, Ownable {
+    address public stakingContract;
+
     constructor() ERC20("Wistake", "SWISTA") {
         _mint(msg.sender, 42000000 * (10 ** uint256(decimals())));
+    }
+
+    function setStakingContract(address _stakingContract) external onlyOwner {
+        stakingContract = _stakingContract;
+    }
+
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        require(msg.sender == owner() || msg.sender == stakingContract, "Wistake: Only allowed from owner or staking contract");
+        _transfer(_msgSender(), recipient, amount);
+        return true;
     }
 }
